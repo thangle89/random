4a0a362e4aabab66f598cf3db46dc1b000601c5e
Thang Le <thangle@Trucs-MacBook-Air.local>
2019-06-29 13:03:52 +0700

    add initial solution with notes

--- OnTB/jobParser/lib/index.ts
+++ OnTB/jobParser/lib/index.ts
@@ -1,3 +1,24 @@
-export default function parse(): void {
-    console.log('test');
+
+interface Job {
+    [name: string]: string[]
+}
+export default function parse(input: string): string {
+    const jobList = input.split(' ');
+    const list = jobList.reduce<Job>((acc, job) => {
+        const dependentJob = job.split('=>');
+        if(acc[dependentJob[0]]) {
+            if(acc[dependentJob[0]].indexOf(dependentJob[1]) === -1){
+                acc[dependentJob[0]] = acc[dependentJob[0]].concat(dependentJob[1]);
+            }
+        }
+        else {
+            acc[dependentJob[0]] = acc[dependentJob[0]].concat(dependentJob[1]);
+        }
+        return acc;
+    }, {} as Job)
+    return Object.keys(list).reduce((str, job) => {
+        let jobStr = list[job].reverse().reduce((a,b) => a + b);
+        jobStr = jobStr + job;
+        return str + jobStr;
+    }, "");
 }
